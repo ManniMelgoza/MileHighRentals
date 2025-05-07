1ed3d4a11ae1b5580ba85db2a6a4c2f69a73d240
Emmanuel <emmanuelmelgoza1@gmail.com>
2025-05-07 14:36:59 -0600

    added ph5 and the option suggestions

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -2,75 +2,77 @@ import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
+// import OpenModalButton from '../OpenModalButton/OpenModalButton';
+import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
 import LoginFormModal from '../../LoginFormModal/LoginFormModal';
 import SignupFormModal from '../../SignupFormModal/SignupFormModal';
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
-  const ulRef = useRef();
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
 
-  const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
-    setShowMenu(!showMenu);
-  };
-
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = (e) => {
-      if (!ulRef.current.contains(e.target)) {
-        setShowMenu(false);
-      }
+    const toggleMenu = (e) => {
+      e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+      setShowMenu(!showMenu);
     };
 
-    document.addEventListener('click', closeMenu);
+    useEffect(() => {
+      if (!showMenu) return;
 
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
+      const closeMenu = (e) => {
+        if (!ulRef.current.contains(e.target)) {
+          setShowMenu(false);
+        }
+      };
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
+      document.addEventListener('click', closeMenu);
 
-  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+      return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
 
-  return (
-    <>
-      <button onClick={toggleMenu}>
-        <FaUserCircle />
-      </button>
-      <ul className={ulClassName} ref={ulRef}>
-        {user ? (
-          <>
-            <li>{user?.username}</li>
-            <li>{user?.firstName} {user?.lastName}</li>
-            <li>{user?.email}</li>
-            <li>
-              <button onClick={logout}>Log Out</button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <OpenModalButton
-                buttonText="Log In"
+    const closeMenu = () => setShowMenu(false);
+
+    const logout = (e) => {
+      e.preventDefault();
+      dispatch(sessionActions.logout());
+      closeMenu();
+    };
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+    return (
+      <>
+        <button onClick={toggleMenu}>
+          <FaUserCircle />
+        </button>
+        <ul className={ulClassName} ref={ulRef}>
+          {user ? (
+            <>
+              <li>{user.username}</li>
+              <li>{user.firstName} {user.lastName}</li>
+              <li>{user.email}</li>
+              <li>
+                <button onClick={logout}>Log Out</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <OpenModalMenuItem
+                itemText="Log In"
+                onItemClick={closeMenu}
                 modalComponent={<LoginFormModal />}
               />
-            </li>
-            <li>
-              <OpenModalButton
-                buttonText="Sign Up"
+              <OpenModalMenuItem
+                itemText="Sign Up"
+                onItemClick={closeMenu}
                 modalComponent={<SignupFormModal />}
               />
-            </li>
-          </>
-        )}
-      </ul>
-    </>
-  );
-}
+            </>
+          )}
+        </ul>
+      </>
+    );
+  }
 
-export default ProfileButton;
+  export default ProfileButton;
