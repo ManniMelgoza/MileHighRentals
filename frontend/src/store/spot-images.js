71e46dfee2403c6a9e58fdcb6f229836229eb0fa
Emmanuel <emmanuelmelgoza1@gmail.com>
2025-05-11 05:17:02 -0600

    added redux store spot-image

---
+++ frontend/src/store/spot-images.js
@@ -0,0 +1,59 @@
+import { csrfFetch } from "./csrf";
+
+// /api/spot-images/:imageId
+// Actions
+const ADD_SPOT_IMAGE = "spot-images/addSpotImage"
+const DELETE_SPOT_IMAGE = "spot-images/deleteSpotImage"
+
+// Action Creators
+const addSpotImageAction = (spotImage) => {
+    return {
+        type: ADD_SPOT_IMAGE,
+        payload: spotImage
+    }
+};
+
+const deleteSpotImageAction = (spotImage) => {
+    return {
+        type: DELETE_SPOT_IMAGE,
+        payload: spotImage
+    }
+};
+
+// Thunk
+
+export const thunkGetSpotImage = (spotsId) => async (dispatch) => {
+    const response = csrfFetch(`/api/spots/${spotsId}/images`);
+    const data = (await response).json();
+    dispatch(addSpotImageAction(data))
+    return response;
+}
+
+
+export const thunkDeleteSpotImage = (imageId) => async (dispatcd) => {
+    const response = await csrfFetch(`/apispots/images/${imageId}`, {
+        method: 'DELETE'
+    });
+    dispatcd(deleteSpotImageAction(imageId));
+    return response;
+}
+// Reducers
+
+const initialState = {
+    spotsImage: {}
+}
+
+const spotImageReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case ADD_SPOT_IMAGE:
+            return { ...state, spotImage: action.payload}
+
+        case DELETE_SPOT_IMAGE:
+            return { ...state, [action.payload.id]: action.payload}
+    default:
+        return state;
+    }
+};
+
+
+export default spotImageReducer;
